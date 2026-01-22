bdc9bc6391ef2d9df74c89f941693d056223cd5c
Logan Sundaram <63442954+logansundaram@users.noreply.github.com>
2026-01-22 12:21:05 -0800

    enabled analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +33,7 @@ export default function RootLayout({
         <Navbar/>
 
         {children}
+        <Analytics/>
 
         <Footer/>
       </body>
