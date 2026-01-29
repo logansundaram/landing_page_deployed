ea2db1f9479f7b601020522516115ecddaf77b8b
Logan Sundaram <63442954+logansundaram@users.noreply.github.com>
2026-01-29 12:21:56 -0800

    polish

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,8 +1,8 @@
 export default function Hero(){
     return(
-        <div className="flex w-full py-20 md:py-40">      
+        <div className="flex w-full py-20 md:py-40">     
             <div>
-                <h1 className="text-zinc-900 text-4xl md:text-6xl lg:text-8xl opacity-0 animate-[fadeUp_0.5s_ease-out_forwards]">
+                <h1 className="text-zinc-900 text-4xl md:text-6xl lg:text-9xl opacity-0 animate-[fadeUp_0.5s_ease-out_forwards]">
                     AI Reimagined.
                 </h1>
                 <p className="text-xl md:text-2xl text-accent opacity-0 animate-[fadeUp_0.7s_ease-out_forwards] [animation-delay:500ms]">
